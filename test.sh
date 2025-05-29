f73e2df9b6f85ccf97ab740edb0efd22935a5a7f
Koschei <nitianzero@gmail.com>
2025-05-29 21:35:20 +0800

    perf: use siftsmall dataset to speed up ci

--- test.sh
+++ test.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Step 1: 下载 SIFT 数据集
-wget -q -O sift.tar.gz ftp://ftp.irisa.fr/local/texmex/corpus/sift.tar.gz
+wget -q -O siftsmall.tar.gz ftp://ftp.irisa.fr/local/texmex/corpus/siftsmall.tar.gz
 
 # Step 2: 解压数据集
-tar -xvzf sift.tar.gz
+tar -xvzf siftsmall.tar.gz
 
 # Step 3: 创建并进入 build 目录
 mkdir -p build
@@ -15,4 +15,4 @@ cmake ..
 cmake --build . --config Release
 
 # Step 5: 执行程序
-./DeepSearch ../sift/sift_base.fvecs ../sift/sift_query.fvecs ../sift/sift_groundtruth.ivecs graph.index 1 10 120 0
+./DeepSearch ../siftsmall/siftsmall_base.fvecs ../siftsmall/siftsmall_query.fvecs ../siftsmall/siftsmall_groundtruth.ivecs graph.index 1 10 120 0
